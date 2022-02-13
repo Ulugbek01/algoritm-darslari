0edf836a9e1692f5ddb3e9e20d32ad44a896e29c
Ulugbek01 <xyz207820@gmail.com>
2022-02-13 20:16:15 +0500

    Binary search algoritmi

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -34,21 +34,32 @@ console.log(linearSearch(myList, -1));
 
 // Binary Search algoritmi
 
-let myList = [1, 3, 4, 6, 7, 8, 10, 12, 23, 45, 56, 78, 99];
 // 1, 2, 3, 4, 5, 6, 7
 //  6 > 4
 // 6
 
 //  4 5 6 7
+let myList = [1, 3, 4, 6, 7, 8, 10, 12, 23, 45, 56, 78, 99];
 
-function binarySearch(arr, key) {
-  for (let i = 0; i < arr.length; i++) {
-    let middleIndex = (i + arr.length - 1) / 2;
-    if (key > arr[middleIndex]) {
-      i = middleIndex;
+function binarySearch(sortedArray, key) {
+  let start = 0;
+  let end = sortedArray.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+
+    if (sortedArray[middle] === key) {
+      // found the key
+      return middle;
+    } else if (sortedArray[middle] < key) {
+      // continue searching to the right
+      start = middle + 1;
+    } else {
+      // search searching to the left
+      end = middle - 1;
     }
   }
-  return "None";
+  // key wasn't found
+  return -1;
 }
-
-console.log(binarySearch(myList, 99));
+console.log(binarySearch(myList, 1));
